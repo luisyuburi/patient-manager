075e0f6c31498a5ba94fbe58d11669e9a6dceab6
Luis Yuburi <luisyuburi@outlook.com>
2023-05-06 19:44:56 -0400

    fixing stuffs 2

--- src/components/ListadoPacientes/ListadoPacientes.jsx
+++ src/components/ListadoPacientes/ListadoPacientes.jsx
@@ -5,7 +5,7 @@ Paciente;
 const ListadoPacientes = (props) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-auto">
-      {props.pacientes && props.pacientes.length ? (
+      {props.pacientes && props.pacientes.length > 0 ? (
         <>
           <h2 className="font-black text-3xl text-center">Listado pacientes</h2>
           <p className="text-xl mt-5 mb-10 text-center">
